049103cd30e010d3389b0e50107f92e4f010b6ac
Ayush Mangal <mangal.ayush.4982@gmail.com>
2025-07-02 22:42:36 +0530

    spotlightcard and problems section

--- src/components/Problems.tsx
+++ src/components/Problems.tsx
@@ -1,8 +1,13 @@
+import SpotlightCard from "./ui/spotlightCard";
+
 const ProblemCard = ({ icon, title, description }: { icon: string, title: string, description: string }) => {
   return (
-    <div className="bg-bgLight p-8 rounded-lg border border-white/10 transition-transform hover:-translate-y-2.5 hover:shadow-2xl">
-      <div className="text-4xl mb-4">{icon}</div>
-      <h3 className="font-heading text-xl text-primaryAccent mb-2.5">{title}</h3>
+    <div className="bg-bgLight rounded-lg transition-transform">
+      <div className="flex items-center gap-4">
+        <div className="text-2xl">{icon}</div>
+        <h3 className="avalon-bold text-xl text-primaryAccent">{title}</h3>
+      </div>
+      <hr className="my-4 border-gray-600" />
       <p className="text-textMedium">{description}</p>
     </div>
   );
@@ -29,16 +34,17 @@ const Problems = () => {
 
   return (
     <section id="problems" className="py-20">
-      <div className="container mx-auto max-w-7xl px-5">
-        <h2 className="font-heading text-4xl text-center mb-10">Is Your Business Feeling the Strain?</h2>
+      <div className="container mx-auto max-w-7xl px-5 h-full flex items-center flex-col justify-center">
+        <h2 className="avalon-bold text-4xl text-center mb-10">Is Your Business Feeling the <span className="dual-underline primary-accent">Strain</span>?</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {problems.map((problem, index) => (
+            <SpotlightCard key={index} className="custom-spotlight-card">
             <ProblemCard 
-              key={index}
               icon={problem.icon}
               title={problem.title}
               description={problem.description}
             />
+            </SpotlightCard>
           ))}
         </div>
       </div>
