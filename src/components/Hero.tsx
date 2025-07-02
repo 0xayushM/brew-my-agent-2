dd46aeca316657701ce45e1aaa2bff5b3abaa5e1
Ayush Mangal <mangal.ayush.4982@gmail.com>
2025-07-03 00:12:58 +0530

    underline change in hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <section id="hero" className="h-screen text-center relative">
       <div className="container mx-auto max-w-7xl px-5 h-full flex items-center flex-col justify-center">
         <h1 className="avalon-bold text-4xl md:text-5xl lg:text-6xl leading-tight max-w-2xl md:max-w-4xl mx-auto mb-5">
-          Your Best Employee is an <span className="dual-underline primary-accent uppercase">AI Agent</span>. We'll <span className="needle-underline primary-accent uppercase">brew</span> one for You.
+          Your Best Employee is an <span className="needle-underline primary-accent uppercase">AI Agent</span>. We'll <span className="needle-underline primary-accent uppercase">brew</span> one for You.
         </h1>
         <p className="text-textMedium text-lg max-w-2xl mx-auto mb-8">
           We provide end-to-end AI consulting and implementation, creating bespoke AI agents that integrate seamlessly into your operations to drive growth and cut costs.
